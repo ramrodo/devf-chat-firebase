ff50268bc71fe5040d000de423c5f2fe18ff5798
Rodolfo Martínez <rodolfo@agavelab.com>
2017-10-16 09:58:12 -0500

    Modified web-start folder

--- functions/index.js
+++ functions/index.js
@@ -16,10 +16,6 @@
 
 // Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
-
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
 // TODO(DEVELOPER): Write the sendNotifications Function here.
